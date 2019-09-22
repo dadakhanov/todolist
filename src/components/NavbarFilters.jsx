9086b17bd548e968a240975b5ab24f3789655ee1
= <dadakhanov@gmail.com>
2019-09-22 20:04:42 +0300

    navlink active on first load

--- src/components/NavbarFilters.jsx
+++ src/components/NavbarFilters.jsx
@@ -4,8 +4,8 @@ import './NavbarFilters.css'
 
 export const NavbarFilters = () => {
     return <div className="NavbarFilters_LinkHolder">
-        <NavLink to="/active" activeClassName='active'>ACTIVE </NavLink>
-        <NavLink to="/completed" >COMPLETED </NavLink>
+        <NavLink to="/active" isActive={(m,l)=>l.pathname==="/" || m}>ACTIVE</NavLink>
+        <NavLink to="/completed" >COMPLETED</NavLink>
         <NavLink to="/all" >ALL</NavLink>
     </div>
 
